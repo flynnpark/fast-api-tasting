d287dedc326ef3de218fdb5ab5cc7eb16ec04046
Flynn <wphestiraid@gmail.com>
2020-10-11 16:38:20 +0900

    Query Parameters and String Validations: Make it required

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Union
 
 from fastapi import FastAPI, Query
 
@@ -6,7 +6,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: str = Query("fixedquery", min_length=3)):
+async def read_items(q: str = Query(..., min_length=3)):
     results: Dict[str, Union[List[Dict[str, str]], str]] = {
         "items": [{"item_id": "Foo"}, {"item_id": "Bar"}]
     }
