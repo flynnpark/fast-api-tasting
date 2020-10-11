5c97b3a161ee3219171de35483d6bb059a4286f1
Flynn <wphestiraid@gmail.com>
2020-10-11 16:31:08 +0900

    Query Parameters and String Validations

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-from typing import Optional
+from typing import Dict, List, Optional, Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
 
 app = FastAPI()
 
 
-@app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: Optional[str] = None):
-    result = {"item_id": item_id, **item.dict()}
+@app.get("/items/")
+async def read_items(q: Optional[str] = None):
+    results: Dict[str, Union[List[Dict[str, str]], str]] = {
+        "items": [{"item_id": "Foo"}, {"item_id": "Bar"}]
+    }
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    return results
